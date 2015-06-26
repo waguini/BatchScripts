9d413cb64a587d63fed17c3ac80137324b44d904
waguini <wagnernunescv@gmail.com>
2015-06-25 23:13:54 -0300

    Batch File for Organizing files
    Quick and dirty batch script to organize your files in folders.

---
+++ Bat-Organizer.bat
@@ -0,0 +1,59 @@
+@echo off
+
+::Author  Wagner Nunes Ribeiro
+
+::color 0A
+cls
+echo.
+echo 			Bat-Organizer
+pause
+
+
+
+::lists containing the filetypes
+set "imagelist=*.jpg *.png *.gif *.jpeg *.bmp *.TIF"
+set "docslist=*.docx *.doc *.xls *.xlsx *.odf *.rtf"
+set "pdfslist=*.pdf"
+set "zipslist=*.zip *.rar *.7z *.tar *.ace"
+set "binlist=*.exe"
+set "videoslist=*.avi *.rmvb *.wmv *.mp4 *.3gp *.mp4 *.flv *.mkv *.mov *.rm *.mpg *.mpeg"
+set "torrentslist=*.torrent"
+
+::end of lists
+
+call:Organize "Torrents",    "%torrentslist%"
+call:Organize "Binaries", "%binlist%"
+call:Organize "Images", 	  "%imagelist%"
+call:Organize "Documents",  "%docslist%"
+call:Organize "PDFS", 		  "%pdfslist%"
+call:Organize "Zips", 		  "%zipslist%"
+call:Organize "Videos", 	  "%videoslist%" 
+
+
+::::::::::::::::::::
+::function session::
+::::::::::::::::::::
+
+:Organize
+
+IF EXIST %~1 (
+echo.
+echo ::Moving %~1...
+for %%B in (%~2) do (
+move "%%B" %~1\ 2>nul
+)
+)else (
+echo.
+echo Creating Directory...
+mkdir %~1 2>nul
+echo.
+echo ::Moving %~1...
+
+for %%B in (%~2) do (
+move "%%B" %~1\ 2>nul
+)
+)
+:: Removes empty folders
+rmdir %~1 >nul
+
+echo.
