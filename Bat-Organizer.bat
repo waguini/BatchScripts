7809d81d601e5535026f95cadedda15267c37c09
waguini <wagnernunescv@gmail.com>
2015-06-27 18:23:23 -0300

    Updates
    binaries Updated and Music classification created

--- Bat-Organizer.bat
+++ Bat-Organizer.bat
@@ -11,20 +11,21 @@ pause
 
 
 ::lists containing the filetypes
-set "imagelist=*.jpg *.png *.gif *.jpeg *.bmp *.TIF"
+set "imagelist=*.jpg *.png *.gif *.jpeg *.bmp"
 set "docslist=*.docx *.doc *.xls *.xlsx *.odf *.rtf"
 set "pdfslist=*.pdf"
 set "zipslist=*.zip *.rar *.7z *.tar *.ace"
-set "binlist=*.exe"
+set "binlist=*.exe *.msi"
 set "videoslist=*.avi *.rmvb *.wmv *.mp4 *.3gp *.mp4 *.flv *.mkv *.mov *.rm *.mpg *.mpeg"
 set "torrentslist=*.torrent"
+set "musiclist=*.mp3 *.wav *.aac *.ogg"
 
 ::end of lists
 
-call:Organize "Torrents",    "%torrentslist%"
-call:Organize "Binaries", "%binlist%"
+call:Organize "Torrents",     "%torrentslist%"
+call:Organize "Binaries",     "%binlist%"
 call:Organize "Images", 	  "%imagelist%"
-call:Organize "Documents",  "%docslist%"
+call:Organize "Documents",    "%docslist%"
 call:Organize "PDFS", 		  "%pdfslist%"
 call:Organize "Zips", 		  "%zipslist%"
 call:Organize "Videos", 	  "%videoslist%" 
