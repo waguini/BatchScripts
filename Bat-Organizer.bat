51dd52843927134c6ce1d8afdb1fe10ac9394d27
Wagner <wagner.nunes.ribeiro@accenture.com>
2019-10-23 15:19:29 -0300

    Correção de lógica

--- Bat-Organizer.bat
+++ Bat-Organizer.bat
@@ -12,11 +12,11 @@ pause
 
 ::lists containing the filetypes
 set "imagelist=*.jpg *.png *.gif *.jpeg *.bmp "
-set "docslist=*.docx *.doc *.xls *.xlsx *.odf *.rtf *.ppt *.pptx *.odt *.csv"
+set "docslist=*.docx *.doc *.xls *.xlsx *.odf *.rtf *.ppt *.pptx *.odt "
 set "pdfslist=*.pdf *.mobi *.epub"
 set "zipslist=*.zip *.rar *.7z *.tar *.ace *.tar *.tar.gz *.bz2 *.gz *.gzip"
-set "binlist=*.exe *.msi *.bin *.jar"
-set "videoslist=*.avi *.rmvb *.wmv *.mp4 *.3gp *.mp4 *.flv *.mkv *.mov *.rm *.mpg *.mpeg "
+set "binlist=*.exe *.msi *.bin"
+set "videoslist=*.avi *.rmvb *.wmv *.mp4 *.3gp *.mp4 *.flv *.mkv *.mov *.rm *.mpg *.mpeg *.ogg  "
 set "torrentslist=*.torrent"
 set "songlist=*.mp3 *.wav *.aac *.ogg"
 
@@ -29,7 +29,7 @@ call:Organize "Documents",    "%docslist%"
 call:Organize "PDFS", 		  "%pdfslist%"
 call:Organize "Zips", 		  "%zipslist%"
 call:Organize "Videos", 	  "%videoslist%" 
-call:Organize "Songs",		  "%songlist%"	
+call:Organize "Songs"		  "%songlist%"	
 
 ::::::::::::::::::::
 ::function session::
@@ -37,24 +37,17 @@ call:Organize "Songs",		  "%songlist%"
 
 :Organize
 
-IF EXIST %~1 (
-echo.
+    IF NOT EXIST %~1 (
+    echo.
+    echo Creating Directory...
+    mkdir %~1 2>nul
+    echo.
+    )
 echo ::Moving %~1...
-for %%B in (%~2) do (
-move "%%B" %~1\ 2>nul
-)
-)else (
-echo.
-echo Creating Directory...
-mkdir %~1 2>nul
-echo.
-echo ::Moving %~1...
-
-for %%B in (%~2) do (
-move "%%B" %~1\ 2>nul
-)
-)
+    for %%B in (%~2) do (
+        move "%%B" %~1\ 2>nul
+    )
 :: Removes empty folders
-rmdir %~1 >nul
+rmdir %~1 2>nul
 
 echo.
